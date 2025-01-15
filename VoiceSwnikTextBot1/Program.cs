51a713851381cbac0175b94cc2ed8d97fde384a2
Sergey Vedernikov <swnikeus@outlook.com>
2025-01-15 18:11:40 +0500

    Добавьте файлы проекта.

---
+++ VoiceSwnikTextBot1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Text;
+using Telegram.Bot;
+using VoiceSwnikTextBot.Controllers;
+
+namespace VoiceSwnikTextBot1
+{
+    internal class Program
+    {
+        public static async Task Main()
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+
+            // Объект, отвечающий за постоянный жизненный цикл приложения
+            var host = new HostBuilder()
+                .ConfigureServices((hostContext, services) => ConfigureServices(services)) // Задаем конфигурацию
+                .UseConsoleLifetime() // Позволяет поддерживать приложение активным в консоли
+                .Build(); // Собираем
+
+            Console.WriteLine("Сервис запущен");
+            // Запускаем сервис
+            await host.RunAsync();
+            Console.WriteLine("Сервис остановлен");
+        }
+
+        static void ConfigureServices(IServiceCollection services)
+        {
+            // Подключаем контроллеры сообщений и кнопок
+            services.AddTransient<DefaultMessageController>();
+            services.AddTransient<VoiceMessageController>();
+            services.AddTransient<TextMessageController>();
+            services.AddTransient<InlineKeyboardController>();
+
+            // Регистрируем объект TelegramBotClient c токеном подключения
+            services.AddSingleton<ITelegramBotClient>(provider => new TelegramBotClient("8096839111:AAH44MfT6dxoZ7kvLUH3VrdFbJVPaJx2NOk"));
+            // Регистрируем постоянно активный сервис бота
+            services.AddHostedService<Bot>();
+        }
+    }
+}
